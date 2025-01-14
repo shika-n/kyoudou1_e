58a66a9c40ce8834621e34aa54900bfec296602b
Shika <greffinroid@ymail.com>
2025-01-14 11:46:49 +0900

    Update layout style

--- layout.php
+++ layout.php
@@ -11,7 +11,7 @@ $html = <<< ___EOF___
 	</head>
 	<body>
 		<header class="px-4 flex gap-4 md:justify-between items-center bg-slate-300">
-			<button class="md:hidden p-2" onclick="toggleNavMenu()">
+			<button class="md:hidden p-2 rounded-full hover:bg-slate-200" onclick="toggleNavMenu()">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
 					<path fill-rule="evenodd" d="M2 4.75A.75.75 0 0 1 2.75 4h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 4.75ZM2 10a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 10Zm0 5.25a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
 				</svg>
@@ -24,12 +24,12 @@ $html = <<< ___EOF___
 		</header>
 		<main class="mt-8">
 			<div class="flex gap-8 w-full md:w-4/5 m-auto">
-				<div id="navMenu" class="hidden md:flex gap-4 fixed md:static flex-col bg-slate-500 md:bg-transparent top-0 h-screen md:h-min">
-					<a class="w-40 py-2 px-4 bg-slate-300 rounded-md md:hidden" href="#">ユーザー名</a>
-					<a class="w-40 py-2 px-4 bg-slate-300 rounded-md md:hidden" href="logout.php">ログアウト</a>
-					<a class="w-40 py-2 px-4 bg-slate-300 rounded-md" href=".">TOP</a>
-					<a class="w-40 py-2 px-4 bg-slate-300 rounded-md" href="#">プロファイル</a>
-					<a class="w-40 py-2 px-4 bg-slate-300 rounded-md" href="#">ユーザー一覧</a>
+				<div id="navMenu" class="hidden md:flex md:gap-4 divide-y md:divide-none divide-gray-500 fixed md:static flex-col bg-slate-300 md:bg-transparent top-0 h-screen md:h-min shadow-[0px_0px_32px_16px_rgba(0,0,0,0.3)] md:shadow-none">
+					<a class="w-40 py-2 px-4 bg-slate-300 hover:bg-slate-200 md:rounded-md md:hidden" href="#">ユーザー名</a>
+					<a class="w-40 py-2 px-4 bg-slate-300 hover:bg-slate-200 md:rounded-md md:hidden" href="logout.php">ログアウト</a>
+					<a class="w-40 py-2 px-4 bg-slate-300 hover:bg-slate-200 md:rounded-md" href=".">TOP</a>
+					<a class="w-40 py-2 px-4 bg-slate-300 hover:bg-slate-200 md:rounded-md" href="#">プロファイル</a>
+					<a class="w-40 py-2 px-4 bg-slate-300 hover:bg-slate-200 md:rounded-md" href="#">ユーザー一覧</a>
 				</div>
 				<div class="p-4 md:px-8 border border-2 border-black rounded-xl flex-grow">
 					<!-- CONTENT -->
