9bbe11e10f9c7e025ec878dc2a1588a765d9786b
Shika <greffinroid@ymail.com>
2025-01-15 11:10:43 +0900

    Update posts query order by time descending

--- models/posts.php
+++ models/posts.php
@@ -10,7 +10,7 @@ function post(PDO $dbh, $user_id, $title, $content) {
 }
 
 function get_posts(PDO $dbh) {
-	$statement = $dbh->prepare("SELECT * FROM posts JOIN users ON users.user_id = posts.user_id;");
+	$statement = $dbh->prepare("SELECT * FROM posts JOIN users ON users.user_id = posts.user_id ORDER BY created_at DESC;");
 	$statement->execute();
 	return $statement->fetchAll();
 }
