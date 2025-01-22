4ee6dd31ac6c95d11e3ae39284a6805eb27f135b
Shika <greffinroid@ymail.com>
2025-01-22 13:06:30 +0900

    Fix get_posts_by_user query

--- models/posts.php
+++ models/posts.php
@@ -109,7 +109,82 @@ function get_posts(PDO $dbh, $user_id) {
 }
 
 function get_posts_by_user(PDO $dbh, $user_id) {
-	$statement = $dbh->prepare("SELECT * FROM posts JOIN users ON users.user_id = posts.user_id WHERE posts.user_id = ? ORDER BY created_at DESC;");
-	$statement->execute([$user_id]);
+	$statement = $dbh->prepare("
+		WITH RECURSIVE base AS (
+			(
+				SELECT
+					post_id,
+					p.user_id,
+					created_at,
+					title,
+					content,
+					image,
+					updated_at,
+					reply_to,
+					deleted_at,
+					name,
+					nickname,
+					icon,
+					(
+						SELECT COUNT(l.user_id)
+						FROM likes l
+						WHERE l.post_id = p.post_id 
+					) AS 'like_count',
+					(
+						SELECT COUNT(c.post_id)
+						FROM posts c
+						WHERE c.reply_to = p.post_id
+					) AS 'comment_count',
+					EXISTS(
+						SELECT 1
+						FROM likes
+						WHERE p.post_id = post_id AND user_id = :user_id1
+					) AS 'liked_by_user'
+				FROM posts p
+				JOIN users u ON u.user_id = p.user_id
+				WHERE p.reply_to IS NULL AND p.user_id = :user_id2
+				ORDER BY created_at DESC
+				LIMIT 10
+			)
+			UNION ALL
+			(
+				SELECT
+					p.post_id,
+					p.user_id,
+					p.created_at,
+					p.title,
+					p.content,
+					p.image,
+					p.updated_at,
+					p.reply_to,
+					p.deleted_at,
+					u.name,
+					u.nickname,
+					u.icon,
+					(
+						SELECT COUNT(l.user_id)
+						FROM likes l
+						WHERE l.post_id = p.post_id 
+					) AS 'like_count',
+					(
+						SELECT COUNT(c.post_id)
+						FROM posts c
+						WHERE c.reply_to = p.post_id
+					) AS 'comment_count',
+					EXISTS(
+						SELECT 1
+						FROM likes
+						WHERE p.post_id = post_id AND user_id = :user_id3
+					) AS 'liked_by_user'
+				FROM posts p
+				JOIN users u ON u.user_id = p.user_id
+				JOIN base b ON b.post_id = p.reply_to
+			)
+		) SELECT * FROM base ORDER BY reply_to IS NULL, created_at DESC;
+	");
+	$statement->bindParam(":user_id1", $user_id);
+	$statement->bindParam(":user_id2", $user_id);
+	$statement->bindParam(":user_id3", $user_id);
+	$statement->execute();
 	return $statement->fetchAll();
 }
