c1cb27e3866b5e84435060c9e70441f9d4b121be
Shika <greffinroid@ymail.com>
2025-01-23 18:00:13 +0900

    Add authentication check

--- api/get_posts.php
+++ api/get_posts.php
@@ -1,6 +1,13 @@
 <?php
 require_once("../db_open.php");
 require_once("../models/posts.php");
+require_once("../util.php");
+
+session_start();
+if (!is_authenticated()) {
+	echo "403 Forbidden";
+	return http_response_code(403);
+}
 
 header("Content-Type: text/json", true, 200);
-echo json_encode(get_posts($dbh, 1));
+echo json_encode(get_posts($dbh, $_SESSION["user_id"]));
