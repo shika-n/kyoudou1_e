ee39a8391e747fa8355b1fdb98c11cd0fa631e5d
Shika <greffinroid@ymail.com>
2025-01-27 12:35:54 +0900

    Fix liked_by_user query

--- models/posts.php
+++ models/posts.php
@@ -149,7 +149,7 @@ function get_posts(PDO $dbh, $user_id, $limit, $offset) {
 	return $statement->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function get_posts_by_user(PDO $dbh, $user_id) {
+function get_posts_by_user(PDO $dbh, $auth_id, $user_id) {
 	$statement = $dbh->prepare("
 		WITH RECURSIVE base AS (
 			(
@@ -179,11 +179,11 @@ function get_posts_by_user(PDO $dbh, $user_id) {
 					EXISTS(
 						SELECT 1
 						FROM likes
-						WHERE p.post_id = post_id AND user_id = :user_id1
+						WHERE p.post_id = post_id AND user_id = :auth_id1
 					) AS 'liked_by_user'
 				FROM posts p
 				JOIN users u ON u.user_id = p.user_id
-				WHERE p.reply_to IS NULL AND p.user_id = :user_id2 AND deleted_at IS NULL
+				WHERE p.reply_to IS NULL AND p.user_id = :user_id AND deleted_at IS NULL
 				ORDER BY created_at DESC
 				LIMIT 10
 			)
@@ -215,7 +215,7 @@ function get_posts_by_user(PDO $dbh, $user_id) {
 					EXISTS(
 						SELECT 1
 						FROM likes
-						WHERE p.post_id = post_id AND user_id = :user_id3
+						WHERE p.post_id = post_id AND user_id = :auth_id2
 					) AS 'liked_by_user'
 				FROM posts p
 				JOIN users u ON u.user_id = p.user_id
@@ -223,9 +223,9 @@ function get_posts_by_user(PDO $dbh, $user_id) {
 			)
 		) SELECT * FROM base ORDER BY reply_to IS NULL, created_at DESC;
 	");
-	$statement->bindParam(":user_id1", $user_id);
-	$statement->bindParam(":user_id2", $user_id);
-	$statement->bindParam(":user_id3", $user_id);
+	$statement->bindParam(":auth_id1", $auth_id);
+	$statement->bindParam(":auth_id2", $auth_id);
+	$statement->bindParam(":user_id", $user_id);
 	$statement->execute();
 	return $statement->fetchAll(PDO::FETCH_ASSOC);
 }
--- profile.php
+++ profile.php
@@ -19,7 +19,7 @@ $user = get_user_by_id($dbh, $target_id);
 $icon = $user["icon"];
 $name = htmlspecialchars($user["name"], ENT_QUOTES, "UTF-8");
 $nickname = htmlspecialchars($user["nickname"], ENT_QUOTES, "UTF-8");
-$post_arr = get_posts_by_user($dbh, $target_id);
+$post_arr = get_posts_by_user($dbh, $_SESSION["user_id"], $target_id);
 
 if (count($post_arr) === 0) {
     // 記事がない場合のメッセージ
