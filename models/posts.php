024ff9239f83c08c1a2d4ca7277c44666a3917d3
Shika <greffinroid@ymail.com>
2025-02-04 10:04:37 +0900

    Display tags on posts
    Simplify query for non-post detail page
    Hide comment section for non-psot detail page

--- models/posts.php
+++ models/posts.php
@@ -86,83 +86,45 @@ function get_posts(PDO $dbh, $user_id, $limit, $offset, $sort_order) {
 		$sort_order_inject = "like_count DESC, ";
 	}
 	$statement = $dbh->prepare("
-		WITH base AS (
-			SELECT
-				post_id,
-				p.user_id,
-				created_at,
-				title,
-				content,
-				image,
-				updated_at,
-				reply_to,
-				deleted_at,
-				name,
-				nickname,
-				icon,
-				(
-					SELECT COUNT(l.user_id)
-					FROM likes l
-					WHERE l.post_id = p.post_id 
-				) AS 'like_count',
-				(
-					SELECT COUNT(c.post_id)
-					FROM posts c
-					WHERE c.reply_to = p.post_id AND c.deleted_at IS NULL
-				) AS 'comment_count',
-				EXISTS(
-					SELECT 1
-					FROM likes
-					WHERE p.post_id = post_id AND user_id = :user_id1
-				) AS 'liked_by_user'
-			FROM posts p
-			JOIN users u ON u.user_id = p.user_id
-			WHERE p.reply_to IS NULL AND deleted_at IS NULL
-			ORDER BY $sort_order_inject created_at DESC
-			LIMIT $limit OFFSET $offset
-		)
-		(
-			SELECT * FROM base
-		)
-		UNION ALL
-		(
-			SELECT
-				p.post_id,
-				p.user_id,
-				p.created_at,
-				p.title,
-				p.content,
-				p.image,
-				p.updated_at,
-				p.reply_to,
-				p.deleted_at,
-				u.name,
-				u.nickname,
-				u.icon,
-				(
-					SELECT COUNT(l.user_id)
-					FROM likes l
-					WHERE l.post_id = p.post_id 
-				) AS 'like_count',
-				(
-					SELECT COUNT(c.post_id)
-					FROM posts c
-					WHERE c.reply_to = p.post_id AND c.deleted_at IS NULL
-				) AS 'comment_count',
-				EXISTS(
-					SELECT 1
-					FROM likes
-					WHERE p.post_id = post_id AND user_id = :user_id2
-				) AS 'liked_by_user'
-			FROM posts p
-			JOIN users u ON u.user_id = p.user_id
-			JOIN base b ON b.post_id = p.reply_to
-			WHERE p.deleted_at IS NULL
-			ORDER BY created_at ASC
-		)
+		SELECT
+			p.post_id,
+			p.user_id,
+			created_at,
+			title,
+			content,
+			image,
+			updated_at,
+			reply_to,
+			deleted_at,
+			u.name,
+			nickname,
+			icon,
+			(
+				SELECT COUNT(l.user_id)
+				FROM likes l
+				WHERE l.post_id = p.post_id 
+			) AS 'like_count',
+			(
+				SELECT COUNT(c.post_id)
+				FROM posts c
+				WHERE c.reply_to = p.post_id AND c.deleted_at IS NULL
+			) AS 'comment_count',
+			EXISTS(
+				SELECT 1
+				FROM likes
+				WHERE p.post_id = post_id AND user_id = :user_id1
+			) AS 'liked_by_user',
+			GROUP_CONCAT(t.name) AS 'tags'
+		FROM posts p
+		JOIN users u ON u.user_id = p.user_id
+		LEFT OUTER JOIN post_tag pt ON p.post_id = pt.post_id
+		LEFT OUTER JOIN tags t ON pt.tag_id = t.tag_id
+		WHERE p.reply_to IS NULL AND deleted_at IS NULL
+		GROUP BY p.post_id
+		ORDER BY $sort_order_inject created_at DESC
+		LIMIT $limit OFFSET $offset
 	");
 	$statement->bindParam(":user_id1", $user_id);
-	$statement->bindParam(":user_id2", $user_id);
 	$statement->execute();
 
 	$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -190,83 +152,45 @@ function get_posts_by_user(PDO $dbh, $auth_id, $user_id, $limit, $offset, $sort_
 		$sort_order_inject = "like_count DESC, ";
 	}
 	$statement = $dbh->prepare("
-		WITH base AS (
-			SELECT
-				post_id,
-				p.user_id,
-				created_at,
-				title,
-				content,
-				image,
-				updated_at,
-				reply_to,
-				deleted_at,
-				name,
-				nickname,
-				icon,
-				(
-					SELECT COUNT(l.user_id)
-					FROM likes l
-					WHERE l.post_id = p.post_id 
-				) AS 'like_count',
-				(
-					SELECT COUNT(c.post_id)
-					FROM posts c
-					WHERE c.reply_to = p.post_id AND c.deleted_at IS NULL
-				) AS 'comment_count',
-				EXISTS(
-					SELECT 1
-					FROM likes
-					WHERE p.post_id = post_id AND user_id = :auth_id1
-				) AS 'liked_by_user'
-			FROM posts p
-			JOIN users u ON u.user_id = p.user_id
-			WHERE p.reply_to IS NULL AND p.user_id = :user_id AND deleted_at IS NULL
-			ORDER BY $sort_order_inject created_at DESC
-			LIMIT :limit OFFSET :offset
-		)
-		(
-			SELECT * FROM base
-		)
-		UNION ALL
-		(
-			SELECT
-				p.post_id,
-				p.user_id,
-				p.created_at,
-				p.title,
-				p.content,
-				p.image,
-				p.updated_at,
-				p.reply_to,
-				p.deleted_at,
-				u.name,
-				u.nickname,
-				u.icon,
-				(
-					SELECT COUNT(l.user_id)
-					FROM likes l
-					WHERE l.post_id = p.post_id 
-				) AS 'like_count',
-				(
-					SELECT COUNT(c.post_id)
-					FROM posts c
-					WHERE c.reply_to = p.post_id AND c.deleted_at IS NULL
-				) AS 'comment_count',
-				EXISTS(
-					SELECT 1
-					FROM likes
-					WHERE p.post_id = post_id AND user_id = :auth_id2
-				) AS 'liked_by_user'
-			FROM posts p
-			JOIN users u ON u.user_id = p.user_id
-			JOIN base b ON b.post_id = p.reply_to
-			WHERE p.deleted_at IS NULL
-			ORDER BY created_at ASC
-		)
+		SELECT
+			p.post_id,
+			p.user_id,
+			created_at,
+			title,
+			content,
+			image,
+			updated_at,
+			reply_to,
+			deleted_at,
+			u.name,
+			nickname,
+			icon,
+			(
+				SELECT COUNT(l.user_id)
+				FROM likes l
+				WHERE l.post_id = p.post_id 
+			) AS 'like_count',
+			(
+				SELECT COUNT(c.post_id)
+				FROM posts c
+				WHERE c.reply_to = p.post_id AND c.deleted_at IS NULL
+			) AS 'comment_count',
+			EXISTS(
+				SELECT 1
+				FROM likes
+				WHERE p.post_id = post_id AND user_id = :auth_id1
+			) AS 'liked_by_user',
+			GROUP_CONCAT(t.name) AS 'tags'
+		FROM posts p
+		JOIN users u ON u.user_id = p.user_id
+		LEFT OUTER JOIN post_tag pt ON p.post_id = pt.post_id
+		LEFT OUTER JOIN tags t ON pt.tag_id = t.tag_id
+		WHERE p.reply_to IS NULL AND p.user_id = :user_id AND deleted_at IS NULL
+		GROUP BY p.post_id
+		ORDER BY $sort_order_inject created_at DESC
+		LIMIT :limit OFFSET :offset
 	");
 	$statement->bindParam(":auth_id1", $auth_id);
-	$statement->bindParam(":auth_id2", $auth_id);
 	$statement->bindParam(":user_id", $user_id);
 	$statement->bindParam(":limit", $limit);
 	$statement->bindParam(":offset", $offset);
