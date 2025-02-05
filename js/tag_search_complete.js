60925a5940b9931525fded12ea11564e546335c4
Shika <greffinroid@ymail.com>
2025-02-05 11:25:25 +0900

    Add search feature

---
+++ js/tag_search_complete.js
@@ -0,0 +1,92 @@
+const searchField = document.getElementById("search-field");
+const suggestionList = document.getElementById("suggestion-list");
+const searchResult = document.getElementById("searchResult");
+
+let selection = -1;
+let suggestions = [];
+
+function searchTags() {
+	const query = searchField.value.split(" ");
+	const params = new URLSearchParams({ "query": query });
+	fetch("api/post_search_by_tag.php?" + params)
+		.then((response) => response.text())
+		.then((text) => {
+			searchResult.innerHTML = text;
+		});
+	
+}
+
+function suggestionItem(value, frequency) {
+	return `
+		<li class="rounded-md px-1 hover:bg-blue-400/30" onclick="complete('${value}')">
+			<div class="flex justify-between gap-2">
+				<span>${value}</span>
+				<span>${frequency}</span>
+			</div>
+		</li>
+	`;
+}
+
+function updateSelection() {
+	for (let i = 0; i < suggestionList.childElementCount; ++i) {
+		const listItem = suggestionList.children[i];
+		if (i == selection) {
+			listItem.classList.add("bg-blue-400");
+		} else {
+			listItem.classList.remove("bg-blue-400");
+		}
+	}
+}
+
+function updateSuggestionsElements(json) {
+	suggestions = json;
+	suggestionList.innerHTML = "";
+	selection = -1;
+	for (let i = 0; i < suggestions.length; ++i) {
+		suggestionList.insertAdjacentHTML("beforeend", suggestionItem(suggestions[i]["name"], suggestions[i]["frequency"]));
+	}
+	suggestionList.classList.remove("hidden");
+	suggestionList.classList.add("absolute");
+}
+
+function complete(suggestion) {
+	searchField.value = searchField.value.substring(0, searchField.value.lastIndexOf(" ") + 1) + suggestion["name"];
+	updateSuggestions([]);
+	searchField.focus();
+}
+
+function updateSuggestions(searchValue) {
+	fetch("api/tag_search.php?search=" + searchValue)
+		.then((response) => response.json())
+		.then((json) => updateSuggestionsElements(json));
+}
+
+searchField.addEventListener("keypress", (e) => {
+	updateSuggestions(searchField.value.substring(searchField.value.lastIndexOf(" ") + 1) + e.key);
+});
+
+searchField.addEventListener("keydown", (e) => {
+	if (e.key == "ArrowDown") {
+		e.preventDefault();
+		selection = Math.max(-1, Math.min(selection + 1, suggestions.length - 1));
+		updateSelection();
+	} else if (e.key == "ArrowUp") {
+		e.preventDefault();
+		selection = Math.max(-1, Math.min(selection - 1, suggestions.length - 1));
+		updateSelection();
+	} else if (e.key == "Enter") {
+		e.preventDefault();
+		if (selection > -1) {
+			complete(suggestions[selection]);
+		} else {
+			searchTags();
+		}
+	} else if (e.key == "Backspace") {
+		updateSuggestions(searchField.value.substring(searchField.value.lastIndexOf(" ") + 1, searchField.value.length - 1));
+	}
+});
+
+searchField.addEventListener("focusout", (e) => {
+	selection = -1;
+	suggestionList.classList.add("hidden");
+});
--- models/tags.php
+++ models/tags.php
@@ -1,5 +1,20 @@
 <?php
 
+function search_tags(PDO $dbh, $search) {
+	$statement = $dbh->prepare("
+		SELECT
+			name,
+			COUNT(post_id) AS 'frequency'
+		FROM tags t
+		LEFT OUTER JOIN post_tag pt ON t.tag_id = pt.tag_id
+		WHERE name LIKE :search
+		GROUP BY t.tag_id
+	");
+	$statement->bindValue(":search", "%$search%");
+	$statement->execute();
+	return $statement->fetchAll(PDO::FETCH_ASSOC);
+}
+
 function get_tag_id_or_create(PDO $dbh, $name) {
 	$statement = $dbh->prepare("SELECT tag_id FROM tags WHERE name = ? LIMIT 1");
 	$statement->execute([$name]);
