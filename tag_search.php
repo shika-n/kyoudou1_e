005cdf3451e3b26a6003d3916405dc7f84bfdf4a
Shika <greffinroid@ymail.com>
2025-02-06 11:25:06 +0900

    Add sort order and add transition-all to button

--- tag_search.php
+++ tag_search.php
@@ -1,10 +1,12 @@
 <?php
 require_once("layout.php");
+require_once("templates.php");
 
-$content = <<< ___EOF___
+$content = sort_order_select();
+$content .= <<< ___EOF___
 	<div class="relative flex gap-2">
 		<input type="search" id="search-field" class="flex-grow px-2 py-1 border border-gray-400 outline-none rounded-md">
-		<button type="button" onclick="searchTags()" class="px-4 py-1 rounded-md bg-slate-300 hover:bg-slate-200 active:bg-slate-400">検索</button>
+		<button type="button" onclick="searchTags()" class="px-4 py-1 rounded-md bg-slate-300 hover:bg-slate-200 active:bg-slate-400 transition-all">検索</button>
 		<ol id="suggestion-list" class="hidden absolute top-8 p-1 bg-white/30 rounded-md border border-gray-400 shadow-xl backdrop-blur-md text-sm"></ol>
 	</div>
 	<div id="searchResult" class="flex flex-col gap-2">
